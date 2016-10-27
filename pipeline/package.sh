c0a5746ab49d066fef83769289e03a604967a10d
Nauman Leghari <nauman.leghari@wipro.com>
2016-10-27 18:07:43 +0100

    ++

--- pipeline/package.sh
+++ pipeline/package.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 
+mkdir some-files
+echo "file1" > some-files/file1
+echo "file2" > some-files/file2
+echo "file3" > some-files/file3
+echo "file4" > some-files/file4
+
+ls some-files/*
+
 mvn -f git-repo/pom.xml -DskipTests=true package
 ls git-repo/target/*
