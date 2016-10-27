64e948d97f2b3ac479456ffb6b301ffa6f43c289
Nauman Leghari <nauman.leghari@wipro.com>
2016-10-27 18:10:14 +0100

    ++

--- pipeline/package.sh
+++ pipeline/package.sh
@@ -6,8 +6,9 @@ echo "file2" > some-files/file2
 echo "file3" > some-files/file3
 echo "file4" > some-files/file4
 
+mvn -f git-repo/pom.xml -DskipTests=true package
+
 mv git-repo/target/concourse-java-maven-test-prj-0.0.1-SNAPSHOT.jar some-files/
 ls some-files/*
-
-mvn -f git-repo/pom.xml -DskipTests=true package
+echo "-----------------"
 ls git-repo/target/*
